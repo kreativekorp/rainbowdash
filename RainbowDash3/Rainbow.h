3d6209cafacc2e6b803489135bdf599aeefbc227
beckiergb@gmail.com <RebeccaRGB@users.noreply.github.com>
2012-03-25 02:51:22 +0000

    RainbowDashboard for Rainbowduino 3.0.

--- RainbowDash3/Rainbow.h
+++ RainbowDash3/Rainbow.h
@@ -1,11 +1,9 @@
 #ifndef RAINBOW_H
 #define RAINBOW_H
 
-void timer2_isr(void);
+void timer1_isr(void);
 void init_rainbow(unsigned char * buffer);
 void set_buffer(unsigned char * buffer);
 void set_next_buffer(unsigned char * buffer);
-unsigned char get_gamma(unsigned char level);
-void set_gamma(unsigned char level, unsigned char value);
 
 #endif
