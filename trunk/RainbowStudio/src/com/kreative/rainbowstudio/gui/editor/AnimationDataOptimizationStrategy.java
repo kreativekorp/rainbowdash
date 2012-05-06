6f5ce5f877a63253955c94c6836f1399267d547a
beckiergb@gmail.com <beckiergb@gmail.com@3e5c543c-d930-c8bb-fc8d-33ea4bafe755>
2012-05-06 06:35:39 +0000

    Panel for selecting animated pixel values in RBD editor. Animation data is not optimized for now, so you can't make very complex animations, but it can be easily improved later.

---
+++ trunk/RainbowStudio/src/com/kreative/rainbowstudio/gui/editor/AnimationDataOptimizationStrategy.java
@@ -0,0 +1,12 @@
+package com.kreative.rainbowstudio.gui.editor;
+
+import java.util.List;
+import com.kreative.rainbowstudio.utility.Pair;
+
+public interface AnimationDataOptimizationStrategy {
+	public void optimizeAnimationData(
+			int count, List<Pair<Integer,byte[]>> data,
+			int[] address, int[] length, int[] offset,
+			byte[] animationData
+	) throws AnimationTooComplexException;
+}
--- trunk/RainbowStudio/src/com/kreative/rainbowstudio/gui/editor/EditorPanel.java
+++ trunk/RainbowStudio/src/com/kreative/rainbowstudio/gui/editor/EditorPanel.java
@@ -58,7 +58,7 @@ public class EditorPanel extends JPanel {
 			}
 		};
 		this.editor = new Rainbowduino(backingStore);
-		this.valuePanel = new MasterPVPanel();
+		this.valuePanel = new MasterPVPanel(backingStore);
 		this.uploadDevice = uploadDevice;
 		
 		JButton newButton = new JButton(new ImageIcon(Resources.FILE_NEW_ICON_SMALL));
